5b1170a1ba5077c089a91bc48b3db0f48fd60bd0
cppdecl <cppdecl@gmail.com>
2025-11-18 22:25:31 +0800

    Added paths to other menus

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import MainMenu from "./menus/MainMenu.jsx";
+import PlayMenu from "./menus/PlayMenu.jsx";
+import GameMenu from "./menus/GameMenu.jsx";
+import ScoreMenu from "./menus/ScoreMenu.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/" element={<MainMenu />} />
+        <Route path="/" element={<MainMenu />} />
+        <Route path="/play" element={<PlayMenu />} />
+        <Route path="/game" element={<GameMenu />} />
+        <Route path="/score" element={<ScoreMenu />} />
       </Routes>
     </Router>
   );
